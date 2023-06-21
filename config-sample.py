74c99bf3cb4265d24f2042b952a98b93dd2950eb
Enrique Barcelli <e.barcelli@gmail.com>
2023-06-22 00:51:13 +0800

    Add Banqup API parameters in config.py

--- config-sample.py
+++ config-sample.py
@@ -9,3 +9,9 @@ userpass_b64 = "cm9ib3RhcGk6S2kxMzAxa28j"
 # Banqup API parameters
 banqup_client_id = 73223
 banqup_platform_id = 15
+bq_client_id = "your-client-id-here"
+bq_client_secret = "your-secret-here"
+bq_base_url = "https://v4-api.platform.eu.banqup.com/"
+bq_auth_url = "https://iam.unifiedpost.com/auth/realms/upg-dev-portal/protocol/openid-connect/auth"
+bq_access_token_url = "https://iam.unifiedpost.com/auth/realms/upg-dev-portal/protocol/openid-connect/token"
+bq_redirect_uri = "https://v4-api.platform.eu.banqup.com/oauth2-redirect.html"
