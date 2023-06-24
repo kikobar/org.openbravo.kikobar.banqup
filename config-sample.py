6ab5c1e4584b5535b5c7b7b34f33998482df677a
Enrique Barcelli <e.barcelli@gmail.com>
2023-06-25 00:51:24 +0800

    Edit config-sample.py

--- config-sample.py
+++ config-sample.py
@@ -3,17 +3,15 @@ CACHE = {}
 # Openbravo API parameters
 
 ob_api_url = "http://[your-openbravo-domain]/openbravo/org.openbravo.service.json.jsonrest/"
-#client_id = "7B75C13C21B945AB9DA2B32DD993FE70"
-#client_identifier = "Grupo Acme Companies"
 userpass_b64 = "cm9ib3RhcGk6S2kxMzAxa28j"
 
 
 # Banqup API parameters
-banqup_client_id = 73223
+banqup_client_id = "73223"
 banqup_platform_id = 15
 bq_client_id = "your-client-id-here"
 bq_client_secret = "your-secret-here"
-bq_base_url = "https://v4-api.platform.eu.banqup.com/"
+bq_base_url = "https://v4-api.platform.eu.banqup.com/v4"
 bq_auth_url = "https://iam.unifiedpost.com/auth/realms/upg-dev-portal/protocol/openid-connect/auth"
 bq_access_token_url = "https://iam.unifiedpost.com/auth/realms/upg-dev-portal/protocol/openid-connect/token"
 bq_redirect_uri = "https://v4-api.platform.eu.banqup.com/oauth2-redirect.html"
