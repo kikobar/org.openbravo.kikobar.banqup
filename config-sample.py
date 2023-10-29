412283e52ff17cbbd7b76a27298f09525325b429
Enrique Barcelli <kiko@barcelli.net>
2023-10-29 18:17:38 +0800

    Add option for configuring openbravo with user and password in clear text

--- config-sample.py
+++ config-sample.py
@@ -1,9 +1,16 @@
+import base64
 CACHE = {}
 
 # Openbravo API parameters
 
 ob_api_url = "http://[your-openbravo-domain]/openbravo/org.openbravo.service.json.jsonrest/"
-userpass_b64 = "cm9ib3RhcGk6S2kxMzAxa28j"
+# Keep only one of the 2 sections below and comment out the other:
+# 1) you may enter the Openbravo user and password
+ob_user = 'your-ob-user'
+ob_pass = 'your-ob-password'
+userpass_b64 = base64.b64encode((ob_user+':'+ob_pass).encode('ascii')).decode('ascii')
+# 2) you may enter the user:password base64 encoded
+#userpass_b64 = "your-ob-user:your-ob-password-base64-encoded"
 
 
 # Banqup API parameters
